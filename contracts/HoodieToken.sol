57725fb10777e028b1dd821b7c000a5056dccb44
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2019-10-09 10:41:38 +1100

    instantiate rTokenContract

--- contracts/HoodieToken.sol
+++ contracts/HoodieToken.sol
@@ -2,6 +2,11 @@ pragma solidity ^0.5.0;
 import "./IRToken.sol";
 
 contract HoodieToken {
+  // RToken address on rinkeby
+  address RTokenAddress = 0x4f3E18CEAbe50E64B37142c9655b3baB44eFF578;
+  IRToken rTokenContract = IRToken(RTokenAddress);
+
+  // Basic set up for FDH
   string public name = "Flex Dapps Hoodie Token";
   string public symbol = "FDH";
   string public standard = "FDH Token v1.0";
