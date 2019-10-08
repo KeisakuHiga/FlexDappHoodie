a035fefc285af6dd835bf7c001ae0b02542a46e1
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2019-10-08 14:58:45 +1100

    use nomal bootstrap instead of react-bootstrap

--- client/src/components/TokenForm.js
+++ client/src/components/TokenForm.js
@@ -1,31 +1,14 @@
 import React from 'react';
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button';
 
 const TokenForm = () => {
   return (
-    <>
-      <Form>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-    </>
+    <form>
+      <div className="form-group">
+        <label htmlFor="exampleInputEmail1">How much DAI will you invest</label>
+        <input type="number" className="form-control" id="inputDAI" placeholder="DAI" />
+      </div>
+      <button type="submit" className="btn btn-primary">Submit</button>
+    </form>
   );
 }
 
